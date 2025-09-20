8af8396f8b024188ff58c03b9eca46956dff8c20
akshdzn <222266665+akshdzn@users.noreply.github.com>
2025-09-20 18:51:15 +0530

    init + ui

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+</script>
+
+<main>
+  <div class="nav">
+    <!-- title -->
+    <div class="titlebx">
+      <div class="title">
+        <div class="text-f">ENCORE</div>
+        <img src="/assets/shape.svg" alt="encore logo" />
+      </div>
+      <button aria-label="About">ABOUT</button>
+    </div>
+    <!-- tools -->
+    <div class="toolbx">
+      <div class="option">
+        <div class="text-f">SHIFT</div>
+        <button class="optionBtn">3</button>
+      </div>
+      <div class="option">
+        <div class="text-f">CIPHER</div>
+        <button class="optionBtn">Caesar</button>
+      </div>
+      <button aria-label="Copy">COPY</button>
+    </div>
+  </div>
+  <div class="content">
+    <div class="text-box"></div>
+    <div class="text-box"></div>
+  </div>
+</main>
