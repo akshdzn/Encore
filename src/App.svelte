9e8619a86033aec50551955f31a2052a57dccb9d
akshdzn <222266665+akshdzn@users.noreply.github.com>
2025-09-20 22:17:05 +0530

    added caesar code

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,47 @@
 <script>
+  import { onMount } from "svelte";
+
+  // mode determination
+  let currentMode = 1;
+  let encodeArea;
+  let decodeArea;
+
+  function setMode(mode) {
+    if (mode == 1) {
+      currentMode = 1;
+    } else {
+      currentMode = 0;
+    }
+    console.log(currentMode);
+  }
+
+  // Caesar Cipher
+  let cipherCode;
+
+  function caesarCipher(str, shift) {
+    // Ensure shift is between 0–25
+    shift = shift % 26;
+
+    return str
+      .split("")
+      .map((char) => {
+        let code = char.charCodeAt(0);
+
+        // Uppercase A–Z
+        if (code >= 65 && code <= 90) {
+          return String.fromCharCode(((code - 65 + shift + 26) % 26) + 65);
+        }
+        // Lowercase a–z
+        else if (code >= 97 && code <= 122) {
+          return String.fromCharCode(((code - 97 + shift + 26) % 26) + 97);
+        }
+        // symbols
+        else {
+          return char;
+        }
+      })
+      .join("");
+  }
 </script>
 
 <main>
@@ -9,13 +52,13 @@
         <div class="text-f">ENCORE</div>
         <img src="/assets/shape.svg" alt="encore logo" />
       </div>
-      <button aria-label="About">ABOUT</button>
+      <button aria-label="About">GITHUB</button>
     </div>
     <!-- tools -->
     <div class="toolbx">
       <div class="option">
         <div class="text-f">SHIFT</div>
-        <input type="number" min="1" value="1" />
+        <input type="number" min="1" value={currentMode} />
       </div>
       <div class="option">
         <div class="text-f">CIPHER</div>
@@ -25,7 +68,25 @@
     </div>
   </div>
   <div class="content">
-    <div class="text-box"></div>
-    <div class="text-box"></div>
+    <div class="text-box">
+      <textarea
+        name="text"
+        class="text"
+        id="encode"
+        on:click={() => {
+          setMode(1);
+        }}
+      ></textarea>
+    </div>
+    <div class="text-box">
+      <textarea
+        name="text"
+        class="text"
+        id="decode"
+        on:click={() => {
+          setMode(0);
+        }}
+      ></textarea>
+    </div>
   </div>
 </main>
